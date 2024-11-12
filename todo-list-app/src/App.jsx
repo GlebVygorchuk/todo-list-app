deb691c162fd80263abc0c9bd78fb40a0a91b6c4
Глеб Выгорчук <vygorcukgleb@gmail.com>
2024-11-12 19:49:28 +0500

    Made first version of project

---
+++ todo-list-app/src/App.jsx
@@ -0,0 +1,12 @@
+import TaskList from './components/taskList/taskList'
+import './styles/styles.css'
+
+function App() {
+  return (
+    <>
+      <TaskList />
+    </>
+  )
+}
+
+export default App
